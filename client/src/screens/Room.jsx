848effde5010734b117623fe2cd60756733c9335
anuragK-24 <24sept.anurag@gmail.com>
2023-12-21 20:01:22 +0530

    The video stream of the user is shown on screen successfully

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,9 +1,11 @@
 import { useCallback, useEffect, useState } from "react";
 import { useSocket } from "../context/SocketProvider";
+import ReactPlayer from "react-player";
 
 export default function Room() {
   const socket = useSocket();
   const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState(null);
   const handleUserJoined = useCallback(({ email, id }) => {
     console.log(`Email ${email} joined with id ${id}`);
     setRemoteSocketId(id);
@@ -16,10 +18,19 @@ export default function Room() {
     };
   }, [socket, handleUserJoined]);
 
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+    });
+    setMyStream(stream);
+}, []);
   return (
     <div>
       <h1>Room page</h1>
-      <h4>{remoteSocketId?"Connected":"No one in room"}</h4>
+      <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
+      {remoteSocketId && <button onClick={handleCallUser}>Call</button>}
+      {myStream && <ReactPlayer url={myStream} playing /> }
     </div>
   );
 }
