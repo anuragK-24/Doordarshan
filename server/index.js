db9a4786f679bbab5f7b08e273b3f3d1bfde756a
anuragK-24 <24sept.anurag@gmail.com>
2023-12-21 19:15:36 +0530

    The lobby given data is returned from the server then reached to the client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const app = express(cors());
-const port = 3000;
-app.use(express.static('public'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const { Server } = require("socket.io");
+const io = new Server(8000, {
+  cors: true,
+});
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log(`Socket connected `, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketIdToEmailMap.set(socket.id, email);
+    io.to(socket.id).emit("room:join", data);
+  });
+});
